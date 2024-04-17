d330b35b06e1e553f667fea7556b49d8988a8647
Mu00221 <blacklives864@gmail.com>
2024-04-16 20:58:21 -0500

    sadfa

--- src/config/api.js
+++ src/config/api.js
@@ -1,18 +1,17 @@
+import axios from "axios";
+const DEPLOYED = "https://mu-adfgsadfasge.com/";
+const LOCALHOST = "http://localhost:9094";
 
-import axios from 'axios';
-const DEPLOYED='https://mu-afghanculture.com/'
-const LOCALHOST='http://localhost:9094'
-
-export const API_BASE_URL = DEPLOYED
+export const API_BASE_URL = DEPLOYED;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-const token = localStorage.getItem('jwt');
+const token = localStorage.getItem("jwt");
 
-api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-api.defaults.headers.post['Content-Type'] = 'application/json';
+api.defaults.headers.post["Content-Type"] = "application/json";
 
 export default api;
